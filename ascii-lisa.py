57fb339e5c0a50f64735009d078e1d26cbd602c9
Queenvictoria <snow@hollysydney.com>
2019-03-24 21:04:40 +1100

    Initial import.

---
+++ ascii-lisa.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# ascii-lisa.py
+
+"""
+Stolen from Shanshan Wang.
+https://wshanshan.github.io/python/asciiart/
+"""
+
+"""
+@TODO
+
+- [ ] Read file.
+- [ ] Select bit depth.
+- [ ] Convert to ascci.
+- [ ] Resize file.
+- [ ] Pass in some variables.
+- [ ] Save it out.
+- [ ] How many bits depth should I choose?
+- [ ] How wide is the terminal? Fit to that.
+- [ ] Ascii colour on the terminal.
+"""
+
